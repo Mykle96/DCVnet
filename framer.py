11142213e62ea5553740b94b28229db7b6230adf
Espen Grødem <espenkg@stud.ntnu.no>
2022-02-15 09:49:06 +0100

    Added .gitignore and made changes to framer.py

--- framer.py
+++ framer.py
@@ -11,11 +11,11 @@ import os
 # This script extracts frames from videos and stores them as images.
 # The amount frames extraceted varies with the frame rate of the video.
 opencv = "cv2"
-assert opencv not in sys.modules, "{} was not properly imported! Make sure it is installed with {}".format(
-    opencv, "opencv-python")
+#assert opencv not in sys.modules, "{} was not properly imported! Make sure it is installed with {}".format(
+   # opencv, "opencv-python")
 
 
-def frame_maker(videoname: str, newDirectory: str):
+def frame_maker(videoname: str, newDirectory: str, adjConst: float):
     """
     A function for generating images from frames of a given video. The function takes in 
     the name of a video located in the videos folder. It makes a new folder for the new
@@ -36,25 +36,27 @@ def frame_maker(videoname: str, newDirectory: str):
     videopath = "videos/{}".format(videoname)
     print("Extracting video from: {}".format(videopath))
     video = cv2.VideoCapture(videopath)
-    fps = fps_checker(video, version)
+    fps = math.ceil(fps_checker(video, version))
+    fps = fps*adjConst
     assert video.isOpened(
     ), "Something went wrong when opening {}, could not read the frame".format(videoname)
     outputPath = folder_generator(newDirectory)
-
+    
     while success:
+        success, frame = video.read()
         if currentFrame % fps == 0:
-            succsess, frame = video.read()
             if success:
-                cv2.imwrite((outputPath+newDirectory +
+               #cv2.imwrite((outputPath+newDirectory +
+                cv2.imwrite((outputPath+"/"+
                             str(imageNumber)+".jpg"), frame)
             imageNumber += 1
         currentFrame += 1
-
+    print("Finished running. {} images written to {} from video {}".format(imageNumber,outputPath,videoname))
 
 def fps_checker(video, version):
     # Checks the frames per second (fps) in the video.
-    assert type(version) == int(
-    ), "Something went wrong with fetching the openCV version. Expected {}, but got {}".format(int(), version.dtype())
+    
+    assert type(version) == int, "Something went wrong with fetching the openCV version. Expected {}, but got {}".format(int(), type(version))
     if version < 3:
         fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
         print("FPS of video: {}".format(fps))
@@ -72,7 +74,12 @@ def check_openCV_version():
 
 def folder_generator(newDirectory):
     outputPath = "raw-data/{}".format(newDirectory)
-    assert os.path.isfile(
-        outputPath) == True, "{} is already a folder under the raw-data folder. please change the name".format(newDirectory)
+    assert os.path.isfile(outputPath) == False, "{} is already a folder under the raw-data folder. please change the name".format(newDirectory)
     os.mkdir(outputPath)
     return outputPath
+
+
+#frame_maker("Loading and unloading of cargo in the port of Savannah, GA..mp4", "videodata",2)
+#frame_maker("Container ship being loaded_unloaded time-lapse.mp4", "videodata2",0.2)
+#frame_maker("Master.mp4", "videodata",1)
+frame_maker("SUMMARY - Stinis Split-Headblock.mp4", "videodata",1)
