b232f0b62bfc9e47049cee91717331f0a6214569
Mykle <magnusmykle@gmail.com>
2022-05-12 18:15:35 +0200

    Cleaned some code

--- engine/trainer.py
+++ engine/trainer.py
@@ -6,19 +6,27 @@ from engine import Model
 from dataLoader import SINTEFDataset
 from utils.utils import *
 
+# ----- MODELS -----
+POSE = False
+SEG = True
 # ----- HYPERPARAMETERS ------
 BATCH_SIZE = 32
 VAL_BATCH_SIZE = 15
 CLASSES = ["container"]
-
+EPOCHS = 150
+LR = 0.001  # 0.0005
+OPTIMIZER = "Sgd"
+MOMENTUM = 0.9
+WEIGHT_DECAY = 0.0005
+GAMMA = 0.1
+LR_STEP = 3
 # ----- DATASET LOADERS ------
-
-TRAIN_DATA_PATH = ""
-VAL_DATA_PATH = ""
-
+TRAIN_DATA_PATH = "data/dataset"
+VAL_DATA_PATH = "data/val_dataset"
+TEST_DATA_PATH = ""
 # fetch and format the data
-TRAIN_DATA, VAL_DATA = SINTEFDataset(TRAIN_DATA_PATH, pose=False, transform=None), SINTEFDataset(
-    VAL_DATA_PATH, pose=False, transform=None)
+TRAIN_DATA, VAL_DATA = SINTEFDataset(TRAIN_DATA_PATH, pose=POSE, transform=None), SINTEFDataset(
+    VAL_DATA_PATH, pose=POSE, transform=None)
 
 # make batches
 TRAIN, VAL = DataLoader(TRAIN_DATA, batch_size=BATCH_SIZE, shuffle=True), DataLoader(
@@ -27,6 +35,11 @@ TRAIN, VAL = DataLoader(TRAIN_DATA, batch_size=BATCH_SIZE, shuffle=True), DataLo
 
 # ------ START TRAINING ------
 
-networks = Model(classes=CLASSES, pose_estimation=False)
 
-losses = networks.train(TRAIN, VAL)
+networks = Model(classes=CLASSES, segmentation=SEG, pose_estimation=POSE)
+
+losses = networks.train(dataset=TRAIN,
+                        val_dataset=VAL,
+                        epochs=EPOCHS,
+                        learning_rate=LR,
+                        optimizer=OPTIMIZER)
