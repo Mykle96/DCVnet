7303cd81218b6c93815627ea916c3c01c82c9b2b
Mykle <magnusmykle@gmail.com>
2022-05-14 16:09:41 +0200

    Deleted redundant files

---
+++ pipeline.py
@@ -0,0 +1,20 @@
+import torch
+from engine.engine import Model
+from engine.dataLoader import SINTEFDataset
+from torch.utils.data import DataLoader
+
+
+# MAIN PIPELINE FILE FOR RUNNING THE TRAINED NETWORKS
+
+
+# variables
+
+# Load models
+
+# Load real data, or live camera
+
+
+# ransac
+# pose
+
+# print to live camera feed
